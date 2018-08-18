7aceaaac390fffa195d987c1e5503699b2b9c26b
nivred <emaildervin@gmail.com>
2018-08-18 18:51:22 -0500

    added js ability to add new book entry

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,53 @@
-console.log('Hello World')
+// Book Constructor
+function Book(title, author,isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add book to list
+UI.prototype.addBookToList = function(book) {
+    const list = document.querySelector('#book-list');
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert columns
+    row.innerHTML =
+    `<td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">x</a></td>`;
+
+    list.appendChild(row);
+}
+
+// Clear fields
+UI.prototype.clearFields = function() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+}
+
+// Event Listeners
+document.querySelector('#book-form').addEventListener('submit', function(e) {
+    // Get form values
+    const title = document.querySelector('#title').value,
+          author = document.querySelector('#author').value,
+          isbn = document.querySelector('#isbn').value
+
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Add book to list
+    ui.addBookToList(book);
+
+    // Clear fields
+    ui.clearFields();
+
+    e.preventDefault();
+});
